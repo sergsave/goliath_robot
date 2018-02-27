ec00c5a598ca70277aff80954690b59ff46e49d7
Sergey Savenko <sa.savenko92@gmail.com>
2018-02-27 23:10:52 +0700

    change jointStateTimerCallback

--- goliath_motion/src/goliath_motion.cpp
+++ goliath_motion/src/goliath_motion.cpp
@@ -47,9 +47,9 @@ public:
     jnt_pub_ =
         nh_.advertise<sensor_msgs::JointState>("joint_states", JNT_QUEUE_SZ);
 
-    jnt_traj_sub_ =
-        private_nh_.subscribe("command", POS_AND_POSE_QUEUE_SZ,
-                              &GoliathMotion::trajectoryCallback, this);
+    jnt_tim_ = nh_.createTimer(ros::Duration(JNT_ST_TIMER_PERIOD),
+                               &GoliathMotion::jointStateTimerCallback, this);
+    jnt_tim_.stop();
 
     jnt_traj_pub_ = private_nh_.advertise<trajectory_msgs::JointTrajectory>(
         "command", JNT_QUEUE_SZ);
@@ -64,48 +64,77 @@ private:
     JNT_QUEUE_SZ = 20
   };
 
-  void trajectoryCallback(const trajectory_msgs::JointTrajectory& traj)
+  void publishJointState(const trajectory_msgs::JointTrajectory& traj)
   {
-    const double RATE_VAL = 20;
+    point_cnt_ = itteration_cnt_ = 0;
+    goal_traj_ = traj;
+    jnt_tim_.start();
+  }
+
+  void jointStateTimerCallback(const ros::TimerEvent& tim_ev)
+  {
     sensor_msgs::JointState jnt_st;
     static std::vector<double> prev_st;
+    static std::vector<double> delta_pos;
+    static std::size_t itterations_numb = 0;
 
-    jnt_st.name = traj.joint_names;
+    jnt_st.name = goal_traj_.joint_names;
 
-    ros::Rate loop_rate(RATE_VAL);
-
-    for (auto p : traj.points)
+    if (point_cnt_ < goal_traj_.points.size())
     {
-      std::size_t itterations_numb = p.time_from_start.toSec() * RATE_VAL;
-      std::vector<double> delta_pos;
-
-      for (std::size_t j = 0; j != p.positions.size(); j++)
+      if (itteration_cnt_ == 0)
       {
-        delta_pos.push_back(
-            (p.positions[j] - (prev_st.empty() ? 0 : prev_st[j])) /
-            itterations_numb);
+        itterations_numb =
+            goal_traj_.points[point_cnt_].time_from_start.toSec() /
+            JNT_ST_TIMER_PERIOD;
+
+
+        ROS_ERROR_STREAM("cnt = 0" << itterations_numb << prev_st.empty() ? 0xFF : prev_st[2]);
+        for (std::size_t j = 0;
+             j != goal_traj_.points[point_cnt_].positions.size(); j++)
+        {
+          delta_pos.push_back((goal_traj_.points[point_cnt_].positions[j] -
+                               (prev_st.empty() ? 0 : prev_st[j])) /
+                              itterations_numb);
+        }
+        itteration_cnt_++;
       }
-
-      for (std::size_t i = 1; i <= itterations_numb; i++)
+      else if (itteration_cnt_ < itterations_numb)
       {
-        loop_rate.sleep();
         jnt_st.header.stamp = ros::Time::now();
 
-        for (std::size_t j = 0; j != p.positions.size(); j++)
+        for (std::size_t j = 0;
+             j != goal_traj_.points[point_cnt_].positions.size(); j++)
           jnt_st.position.push_back((prev_st.empty() ? 0 : prev_st[j]) +
-                                    i * delta_pos[j]);
+                                    itteration_cnt_ * delta_pos[j]);
 
         publishTransformToGroundFrame();
         jnt_pub_.publish(jnt_st);
         jnt_st.position.clear();
+
+        itteration_cnt_++;
+        prev_st = jnt_st.position;
+        ROS_ERROR_STREAM("cnt < numb" << itterations_numb << prev_st.empty() ? 0xFF : prev_st[2]);
       }
+      else if (itteration_cnt_ == itterations_numb)
+      {
 
-      jnt_st.header.stamp = ros::Time::now();
-      prev_st = jnt_st.position = p.positions;
+        jnt_st.header.stamp = ros::Time::now();
+        prev_st = jnt_st.position = goal_traj_.points[point_cnt_].positions;
+        ROS_ERROR_STREAM("cnt = numb" << itterations_numb << prev_st.empty() ? 0xFF : prev_st[2]);
 
-      jnt_pub_.publish(jnt_st);
-      jnt_st.position.clear();
+        publishTransformToGroundFrame();
+        jnt_pub_.publish(jnt_st);
+        jnt_st.position.clear();
+
+        itteration_cnt_++;
+        point_cnt_++;
+
+        delta_pos.clear();
+      }
     }
+    else
+      jnt_tim_.stop();
   }
 
   void bodyPoseCallback(const goliath_msgs::BodyPose& pose)
@@ -162,11 +191,21 @@ private:
     jnt_traj.header.stamp = ros::Time::now();
     jnt_traj.joint_names = jnt_st.name;
 
-    point.positions = jnt_st.position;
-    point.time_from_start = ros::Duration(0.75);
+    for (auto jnt : jnt_st.position)
+      point.positions.push_back(0.0);
+
+    //point.positions[2] = 0.0;
+    //point.time_from_start = ros::Duration(0.5);
+
+    //jnt_traj.points.push_back(point);
+
+    point.positions[2] = 1.0;
+    point.time_from_start = ros::Duration(0.5);
 
     jnt_traj.points.push_back(point);
+
     jnt_traj_pub_.publish(jnt_traj);
+    publishJointState(jnt_traj);
   }
 
   void publishTransformToGroundFrame()
@@ -196,6 +235,7 @@ private:
                                               "center_of_rotation", "ground"));
   }
 
+  static const double JNT_ST_TIMER_PERIOD;
   ros::NodeHandle nh_;
   ros::NodeHandle private_nh_;
   ros::Publisher jnt_pub_;
@@ -206,11 +246,16 @@ private:
   goliath_msgs::LegsPosition legs_pos_;
 
   ros::Publisher jnt_traj_pub_;
-  ros::Subscriber jnt_traj_sub_;
 
   // use for visualisation
   tf::TransformBroadcaster tf_br_;
+
+  ros::Timer jnt_tim_;
+  trajectory_msgs::JointTrajectory goal_traj_;
+  std::size_t point_cnt_, itteration_cnt_;
 };
+
+const double GoliathMotion::JNT_ST_TIMER_PERIOD = 0.1;
 
 int main(int argc, char** argv)
 {
